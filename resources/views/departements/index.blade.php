23bef2a1feffc14695b1ef5929243ec9abc338c5
AYYOUB BOUSKKAR <ayoubbouskkar@gmail.com>
2026-04-20 22:55:35 +0100

    remove conflis in index departement

--- resources/views/departements/index.blade.php
+++ resources/views/departements/index.blade.php
@@ -149,12 +149,7 @@
                                 </div>
                                 
                                 {{-- Edit Button --}}
-<<<<<<< HEAD
-                                <button type="button" onclick="openEditDeptModal('{{ $dept->idDepartement }}')" class="shrink-0 text-slate-300 hover:text-[#b11d40] transition-colors p-1 mr-1" title="Modifier">
-=======
-                            
                                 <button type="button" onclick="openEditDeptModal('{{ $dept->idDepartement ?? $dept->id }}', '{{ addslashes($dept->title) }}', '{{ addslashes($dept->description) }}', '{{ $dept->idUser }}')" class="shrink-0 text-slate-300 hover:text-[#b11d40] transition-colors p-1 mr-1" title="Modifier">
->>>>>>> 8e34c67924711563494f73d2420702add5056fbd
                                     <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                     </svg>
