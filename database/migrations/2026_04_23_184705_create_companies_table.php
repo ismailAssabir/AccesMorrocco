387857da38b59eb16bcea55d5c22a0bb7f5e3037
ismail <ismailassabir02@gmail.com>
2026-04-23 19:54:43 +0100

    ajout de permission views et migration model de company

---
+++ database/migrations/2026_04_23_184705_create_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('companyGps', 10,7);
+            $table->time('companyEntryTime');
+            $table->time('companyExitTime');
+            $table->decimal('distance',6,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
