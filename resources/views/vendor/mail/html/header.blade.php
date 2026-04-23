b9f802774b6c266e49f9fbb6d160e7a71f2956f4
AYYOUB BOUSKKAR <ayoubbouskkar@gmail.com>
2026-04-23 06:11:30 +0100

    modifications desing mail reset pass

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,11 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    <span style="color: #be123c; font-size: 24px; font-weight: 800; letter-spacing: -0.5px;">ACCESS MOROCCO</span>
+<td class="header" style="padding: 40px 0; text-align: center;">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+    <img src="https://i.ibb.co/Hfm6DJ2Y/access.png" alt="Access Morocco" style="width: 120px; height: auto; border: none; display: block; margin: 0 auto 15px auto;">
+    <span style="color: #b11d40; font-size: 20px; font-weight: 800; letter-spacing: 4px; text-transform: uppercase; display: inline-block; font-family: 'Inter', sans-serif;">
+        ACCESS MOROCCO
+    </span>
 </a>
 </td>
 </tr>
